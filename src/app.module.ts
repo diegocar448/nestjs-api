e06b81aff9274dbf35ef73c2674967a7ed757609
Diego <diegocar448@hotmail.com>
2025-06-21 03:13:06 -0300

    set version

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransactionsModule } from './transactions/transactions.module';
+import { ConfigModule } from '@nestjs/config';
+import { Transaction } from './transactions/entities/transaction.entity';
+// import { TransactionsModule } from './transactions/transactions.module';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: process.env.DB_CONNECTION as any,
       host: process.env.DB_HOST,
@@ -13,7 +17,7 @@ import { TransactionsModule } from './transactions/transactions.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      models: [Account, Transaction, Report],
+      models: [Transaction],
       autoLoadModels: true,
       synchronize: true,
       sync:{
@@ -21,7 +25,7 @@ import { TransactionsModule } from './transactions/transactions.module';
         force: true,
       },
     })
-    //TransactionsModule
+    // TransactionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
