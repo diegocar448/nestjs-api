9e1e0750173dd1c9f753ca2562af9dff0673382f
Diego <diegocar448@hotmail.com>
2025-06-20 21:58:01 -0300

    comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,22 @@ import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
-    // SequelizeModule.forRoot({
-    //   dialect: process.env.DB_CONNECTION as any,
-    //   host: process.env.DB_HOST,
-    //   port: parseInt(process.env.DB_PORT),
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_DATABASE,
-    //   models: [Account, Transaction, Report],
-    //   autoLoadModels: true,
-    //   synchronize: true,
-    //   sync:{
-    //     alter: true,
-    //     force: true,
-    //   },
-    // })
-    TransactionsModule
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [Account, Transaction, Report],
+      autoLoadModels: true,
+      synchronize: true,
+      sync:{
+        alter: true,
+        force: true,
+      },
+    })
+    //TransactionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
