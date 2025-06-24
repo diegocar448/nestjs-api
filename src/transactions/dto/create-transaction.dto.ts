552d895d5d0eddc065d87075b8e8f4397b00a262
Diego <diegocar448@hotmail.com>
2025-06-23 23:22:10 -0300

    atribuir validacoes nos campos

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,13 +1,30 @@
+import { IsISO8601, IsNotEmpty, MaxLength, IsString } from 'class-validator';
 import {
   TransactionCategory,
   TransactionType,
 } from '../entities/transaction.entity';
 
 export class CreateTransactionDto {
+  @IsISO8601() //data no formato "2025/07/05"
+  @IsNotEmpty()
   payment_date: Date;
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
   category: TransactionCategory;
+
+  @IsNotEmpty()
   amount: number;
+
+  @IsNotEmpty()
   type: TransactionType;
 }
